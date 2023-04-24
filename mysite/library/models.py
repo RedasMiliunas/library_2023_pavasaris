8f06ebc6403971e317955e30ab5b495247185e58
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-24 22:56:51 +0300

    Smulkus pakeitimai models.py failiuke (prideti def str metodai)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -7,14 +7,16 @@ class Genre(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=50,
                             help_text="Iveskite knygos zanra (pvz.: Detektyvas)")
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=50)
     last_name = models.CharField(verbose_name="Pavarde", max_length=50)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Book(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=100)
@@ -24,6 +26,9 @@ class Book(models.Model):
     author = models.ForeignKey(to="Author", verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(to="Genre", verbose_name="Zanras")
 
+
+    def __str__(self):
+        return f'{self.title} ({self.author})'
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text="Unikalus ID knygos kopijai")
@@ -39,3 +44,5 @@ class BookInstance(models.Model):
 
     status = models.CharField(verbose_name="Busena", max_length=1, choices=LOAN_STATUS, blank=True, default='a')
 
+    def __str__(self):
+        return f'{self.uuid} ({self.due_back}) - {self.status}'
