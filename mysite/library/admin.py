9462a01bc79a442b911e482daf67af43146e4b7a
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-25 10:27:57 +0300

    3cia paskaita - sukurtas BoodInstanceAdmin su filtrais ir fieldsetais

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -7,10 +7,20 @@ from . import models
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'author', 'display_genre']
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'status', 'due_back']
+    list_filter = ['status', 'due_back', 'book']
+    #nustatom filtra
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
 # Register your models here.
 admin.site.register(models.Author)
 admin.site.register(models.Genre)
 # admin.site.register(models.Book)
 admin.site.register(models.Book, BookAdmin)     # cia turim uzregistruoti su klases pavadinimu per kableli!
-admin.site.register(models.BookInstance)
+# admin.site.register(models.BookInstance)
+admin.site.register(models.BookInstance, BookInstanceAdmin)
 
