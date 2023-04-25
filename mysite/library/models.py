edadd0a2385269be30a5fc995ba3a74ff88c326b
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-25 09:52:05 +0300

    3cia paskaita - prideti META duomenys i modelius (atvaizdavimo pavadinimai)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -10,6 +10,10 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Zanras"
+        verbose_name_plural = "Zanrai"
+
 
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=50)
@@ -17,6 +21,10 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = "Autorius"
+        verbose_name_plural = "Autoriai"
 
 class Book(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=100)
@@ -29,6 +37,10 @@ class Book(models.Model):
 
     def __str__(self):
         return f'{self.title} ({self.author})'
+
+    class Meta:
+        verbose_name = "Knyga"
+        verbose_name_plural = "Knygos"
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text="Unikalus ID knygos kopijai")
@@ -46,3 +58,7 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f'{self.book.title} {self.uuid} ({self.due_back}) - {self.status}'
+
+    class Meta:
+        verbose_name = "Knygos egzempliorius"
+        verbose_name_plural = "Knygos egzemplioriai"
