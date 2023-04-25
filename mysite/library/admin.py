f48c8cad5aa068237d338ca0d8c90aec18453aee
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-25 15:47:52 +0300

    3cia paskaita: prie autoriaus atvaizduojamos visos jo knygos; prie knygu prideta paieska pagal (pavadinima, autoriaus varda ir pavarde); prie knygu egzemplioriu prideta paieska pagal (uuid, knygos pavadinima), ir list_editable.

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -12,22 +12,29 @@ class BooksInstanceInline(admin.TabularInline):
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'author', 'display_genre']
+
     #pridedam eilute cia del inline dalyko:
     inlines = [BooksInstanceInline]
+    search_fields = ['title', 'author__first_name', 'author__last_name']       #ivedam search lauka, __ jeigu norim pasiekti toliau esancius elementus
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['uuid', 'book', 'status', 'due_back']
     list_filter = ['status', 'due_back', 'book']
+    list_editable = ['status', 'due_back']      #jeigu norim padaryti, kad toj pacioj vietoj
+                        # galetumem redaguoti iskart, nereiketu eiti atskirai i BookInstance
+    search_fields = ['uuid', 'book__title']     #galim ideti search'a pagal ka norim, ka galim pasiekti
     #nustatom filtra
     fieldsets = (
         ('General', {'fields': ('uuid', 'book')}),
         ('Availability', {'fields': ('status', 'due_back')}),
     )
 
-
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'display_books']
 
 # Register your models here.
-admin.site.register(models.Author)
+# admin.site.register(models.Author)
+admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Genre)
 # admin.site.register(models.Book)
 admin.site.register(models.Book, BookAdmin)     # cia turim uzregistruoti su klases pavadinimu per kableli!
