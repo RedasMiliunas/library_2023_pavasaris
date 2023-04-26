65da2454e2de017c73d760c1dfb53bcff485cbf7
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-26 15:34:09 +0300

    Padarytas URL nukreipimas (redirect) i 'library/'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('library.urls'))
+    path('', include('library.urls')),
+    # path('library/', include('library.urls')),
+    # path('', RedirectView.as_view(url='library/', permanent=True)),
 ]
 
 #Static dalykai
