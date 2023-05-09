abe7881f374c0cc76891a2010a4f2fd01831aaf9
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-09 13:25:32 +0300

    Pridetas vartotojo ir jo profilio redagavimas front-end'e (plius papildoma else: salyga su 'messages.error()'

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -162,7 +162,15 @@ from .forms import UserUpdateForm, ProfileUpdateForm
 @login_required
 def profile(request):
     if request.method == 'POST':
-        pass
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.info(request, f'Profilis sekmingai atnaujintas!')
+            return redirect('profile')
+        else:
+            messages.error(request, f'Kazka blogai vedi...')
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
